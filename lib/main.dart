c3568218a846abae4b0f3bd613c8ea4e99d58754
Anjalisinghrealcoderz <90308579+Anjalisinghrealcoderz@users.noreply.github.com>
2022-06-24 12:42:38 +0530

    Day 2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
+import 'package:first_flutter_project/home_page.dart';
 import 'package:flutter/material.dart';
 
- void main() {
+void main() {
   runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage());
   }
- class MyApp extends StatelessWidget{
-  Widget build(BuildContext context){
-    return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child:Text("Welcome To 30 Days of Coding") ,
-          ),
-        ),
-      ),
-    );
-  }
- }
+}
